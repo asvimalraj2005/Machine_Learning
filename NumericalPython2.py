1aa9692967f675b5a61ccb3b32145e06d77a6067
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-25 18:15:57 +0530

    Numerical Python 2

---
+++ NumericalPython2.py
@@ -0,0 +1,22 @@
+# Data Types in Python
+# strings --> used to represent text data within double quotes where character can be wrapped around within single quotes 
+# integer --> + infinity to - infinity
+# float   --> decimal point numbers 
+# boolean --> True or False will represented 
+# complex --> Real number representation or complex number representation  -->  1.0 + 2.0 i
+#
+#
+# Data Types in numerical python 
+# Numerical python has some extra data types, and refer to data types with one character, like i for integers and, u for unsigned integers 
+# There are lot of data types are there but only one type is used to create a numerical python array 
+# i - integer              array contains -infinity values to +infinity values
+# b - boolean              array contains 0's and 1's   -> 1 represents True and 0 represents False
+# u - unsigned integer     array contains 32-bit datum that encodes a non-negative integer in the range [0 to 4294967295]
+# f - float                array contains of decimal values 
+# c - complex float        array contains of imaginary values 
+# m - timedelta            array contains of data that have the duration between two events 
+# M - datetime             array contains of data and time values (IDK )
+# O - object               
+# S - String
+# U - unicode string
+# V - fixed chunk of memory for other type (void )
