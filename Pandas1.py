082b86d51f11e739862d5e064e9b43705dd51eb5
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-26 12:48:15 +0530

    Pandas1.py

--- Pandas1.py
+++ Pandas1.py
@@ -3,9 +3,9 @@
 # Numerical Python is for powerful calculation of different types of tensors or scalars or 2-D or N-D dimensions of data (Scientific Computing )
 # >>>>>> pip install pandas 
 # Base 
-# import pandas as pd 
-# df = pd . read_csv ( ' data . csv ')
-# print( df . to_string ( ) ) 
+# import pandas as pd                
+# df = pd . read_csv ( ' data . csv ')             //       loading the CSV data set on to the df named object 
+# print( df . to_string ( ) )                     <-- prints entire dataframe  // -->       print(df)   <-- only print top 5 rows and last 5 rows 
 # 
 # The above 3 lines is base for reading every kind of csv files with any limit of data 
 # 
@@ -63,10 +63,42 @@
 # 
 # import pandas as pd 
 # data = { "timeline" : [ 2036 , 2040 , 2045 ],
-#                       [   1  ,  10  ,  5   ] }
+#           "date"    :  [   1  ,  10  ,  5   ] }
 # myvar = pd.DataFrame(data)
 # print(myvar)
+#                       O/P : timeline   date
+#                              2036      1
+#                              2040      10
+#                              2045       5
+# 
+# Data Frame is consist of rows and columns 
+# Pandas library uses 'loc' attribute to return one or more specified row(s)
+# print(df.loc[0])
+#                       O/P : timeline : 2036
+#                             date     : 1
+#                             Name : 0, dtype: int64 
 #
-#
+# print( df . loc [ [ 0 , 1 ] ] )
+#                       O/P : timeline  time
+#                     0       2036      1
+#                     1       2040      10 
+# 
+# Named Indexes : with the index argument, you can name your own indexes 
+# import pandas as pd  
+# data = { "timeline" : [10,20,30],
+#          "date"     : [1,2,3]   }
+# indexes=["data_1" ,"data_2" , "data_3"]            
+# df = pd . DataFrame(data,indexes)
+# print(df)                                      --->        timeline   data 
+# print(df.loc[10])          -->  1                   data_1    10       1
+#                                                     data_2    20       2
+#                                                     data_3    30       3 
+# 
+# print(df.to_string())      is same as      > pd.options.display.max_rows=10000        (or)   print(df) 
+#                                              df = pd.read_csv('data.csv')                     The above line only prints the 5 top and last bottom rows 
+#                                              print(df)
+# This line is defaultly prints                        
+# all the rows and columns                     The above line modifies the how many
+#                                              lines to get printed on the screen 
 #
 #
